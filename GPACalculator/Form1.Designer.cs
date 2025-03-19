71e75b3aaca56771c43a0d524157d4fb53bd2029
Mdsr2op <mdsrmfkhan@gmail.com>
2025-03-19 11:32:33 +0500

    Implement basic design and functionality for the calculator

--- GPACalculator/Form1.Designer.cs
+++ GPACalculator/Form1.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSemesterCount = new System.Windows.Forms.Label();
+            this.numSemesters = new System.Windows.Forms.NumericUpDown();
+            this.btnGenerateSemesters = new System.Windows.Forms.Button();
+            this.pnlSemesters = new System.Windows.Forms.Panel();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numSemesters)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(177, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "CGPA Calculator";
+            // 
+            // lblSemesterCount
+            // 
+            this.lblSemesterCount.AutoSize = true;
+            this.lblSemesterCount.Location = new System.Drawing.Point(14, 55);
+            this.lblSemesterCount.Name = "lblSemesterCount";
+            this.lblSemesterCount.Size = new System.Drawing.Size(116, 13);
+            this.lblSemesterCount.TabIndex = 1;
+            this.lblSemesterCount.Text = "Number of Semesters:";
+            // 
+            // numSemesters
+            // 
+            this.numSemesters.Location = new System.Drawing.Point(136, 53);
+            this.numSemesters.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numSemesters.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSemesters.Name = "numSemesters";
+            this.numSemesters.Size = new System.Drawing.Size(62, 20);
+            this.numSemesters.TabIndex = 2;
+            this.numSemesters.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnGenerateSemesters
+            // 
+            this.btnGenerateSemesters.Location = new System.Drawing.Point(204, 52);
+            this.btnGenerateSemesters.Name = "btnGenerateSemesters";
+            this.btnGenerateSemesters.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerateSemesters.TabIndex = 3;
+            this.btnGenerateSemesters.Text = "Generate";
+            this.btnGenerateSemesters.UseVisualStyleBackColor = true;
+            this.btnGenerateSemesters.Click += new System.EventHandler(this.btnGenerateSemesters_Click);
+            // 
+            // pnlSemesters
+            // 
+            this.pnlSemesters.AutoScroll = true;
+            this.pnlSemesters.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSemesters.Location = new System.Drawing.Point(17, 129);
+            this.pnlSemesters.Name = "pnlSemesters";
+            this.pnlSemesters.Size = new System.Drawing.Size(350, 200);
+            this.pnlSemesters.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(17, 345);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 30);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate CGPA";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(17, 388);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(139, 17);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "Your CGPA: 0.00";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(247, 345);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(120, 30);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(14, 89);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(328, 26);
+            this.lblInstructions.TabIndex = 8;
+            this.lblInstructions.Text = "Enter your GPA for each semester (between 0.00 and 4.00).\r\nAlso enter the number of credit hours for each semester (between 1 and 30).";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 421);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.pnlSemesters);
+            this.Controls.Add(this.btnGenerateSemesters);
+            this.Controls.Add(this.numSemesters);
+            this.Controls.Add(this.lblSemesterCount);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CGPA Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numSemesters)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSemesterCount;
+        private System.Windows.Forms.NumericUpDown numSemesters;
+        private System.Windows.Forms.Button btnGenerateSemesters;
+        private System.Windows.Forms.Panel pnlSemesters;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblInstructions;
     }
 }
-
--- GPACalculator/Form1.cs
+++ GPACalculator/Form1.cs
@@ -12,9 +12,132 @@ namespace GPACalculator
 {
     public partial class Form1 : Form
     {
+        private List<TextBox> gpaTextBoxes = new List<TextBox>();
+        private List<TextBox> creditHoursTextBoxes = new List<TextBox>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void btnGenerateSemesters_Click(object sender, EventArgs e)
+        {
+            pnlSemesters.Controls.Clear();
+            gpaTextBoxes.Clear();
+            creditHoursTextBoxes.Clear();
+
+            int semesterCount = (int)numSemesters.Value;
+
+            for (int i = 0; i < semesterCount; i++)
+            {
+                Label lblSemester = new Label();
+                lblSemester.Text = string.Format("Semester {0}", i + 1);
+                lblSemester.Location = new Point(10, 10 + i * 60);
+                lblSemester.AutoSize = true;
+                lblSemester.Font = new Font(lblSemester.Font, FontStyle.Bold);
+                pnlSemesters.Controls.Add(lblSemester);
+
+                Label lblGPA = new Label();
+                lblGPA.Text = "GPA:";
+                lblGPA.Location = new Point(20, 35 + i * 60);
+                lblGPA.AutoSize = true;
+                pnlSemesters.Controls.Add(lblGPA);
+
+                TextBox txtGPA = new TextBox();
+                txtGPA.Location = new Point(60, 32 + i * 60);
+                txtGPA.Size = new Size(60, 20);
+                txtGPA.Name = string.Format("txtGPA{0}", i);
+                txtGPA.Tag = i;
+                gpaTextBoxes.Add(txtGPA);
+                pnlSemesters.Controls.Add(txtGPA);
+
+                Label lblCreditHours = new Label();
+                lblCreditHours.Text = "Credit Hours:";
+                lblCreditHours.Location = new Point(160, 35 + i * 60);
+                lblCreditHours.AutoSize = true;
+                pnlSemesters.Controls.Add(lblCreditHours);
+
+                TextBox txtCreditHours = new TextBox();
+                txtCreditHours.Location = new Point(240, 32 + i * 60);
+                txtCreditHours.Size = new Size(60, 20);
+                txtCreditHours.Name = string.Format("txtCreditHours{0}", i);
+                txtCreditHours.Tag = i;
+                creditHoursTextBoxes.Add(txtCreditHours);
+                pnlSemesters.Controls.Add(txtCreditHours);
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (gpaTextBoxes.Count == 0)
+            {
+                MessageBox.Show("Please generate semester input fields first.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            double totalWeightedGPA = 0;
+            double totalCreditHours = 0;
+
+            for (int i = 0; i < gpaTextBoxes.Count; i++)
+            {
+                double semesterGPA;
+                int creditHours;
+
+                if (!double.TryParse(gpaTextBoxes[i].Text, out semesterGPA))
+                {
+                    MessageBox.Show(string.Format("Invalid GPA for Semester {0}. Please enter a valid number.", i + 1), "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    gpaTextBoxes[i].Focus();
+                    return;
+                }
+
+                if (semesterGPA < 0 || semesterGPA > 4.0)
+                {
+                    MessageBox.Show(string.Format("GPA for Semester {0} must be between 0.00 and 4.00.", i + 1), "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    gpaTextBoxes[i].Focus();
+                    return;
+                }
+
+                if (!int.TryParse(creditHoursTextBoxes[i].Text, out creditHours))
+                {
+                    MessageBox.Show(string.Format("Invalid Credit Hours for Semester {0}. Please enter a valid number.", i + 1), "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    creditHoursTextBoxes[i].Focus();
+                    return;
+                }
+
+                if (creditHours < 1 || creditHours > 30)
+                {
+                    MessageBox.Show(string.Format("Credit Hours for Semester {0} must be between 1 and 30.", i + 1), "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    creditHoursTextBoxes[i].Focus();
+                    return;
+                }
+
+                totalWeightedGPA += semesterGPA * creditHours;
+                totalCreditHours += creditHours;
+            }
+
+            double cgpa = totalWeightedGPA / totalCreditHours;
+            lblResult.Text = string.Format("Your CGPA: {0:F2}", cgpa);
+            lblResult.ForeColor = Color.Green;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void ResetForm()
+        {
+            numSemesters.Value = 1;
+            pnlSemesters.Controls.Clear();
+            gpaTextBoxes.Clear();
+            creditHoursTextBoxes.Clear();
+            lblResult.Text = "Your CGPA: 0.00";
+            lblResult.ForeColor = Color.Black;
         }
     }
 }
